92eb526add63d0b8e8d54b0eb27e3150cf556657
hamoci <hamoci@naver.com>
2022-04-15 14:07:23 +0900

    주석 추가 등 마지막 수정

--- tabular.h
+++ tabular.h
@@ -6,23 +6,22 @@
 typedef struct _string_data {
 	std::string minterm;
 	char flag;
-} StringData;
+} StringData;	//Minterm의 종류를 파악. Don'tcare minterm인지 true minterm인지
 
 typedef struct _logic_expression
 {
 	std::vector<StringData> minterm;
 	int minterm_num;
 	int bit_length;
-}LogicExpr;
+}LogicExpr;	//논리식을 저장할 구조체. 비트의 길이와 minterm의 개수 저장
 
 typedef struct _pi_s {
-	std::string PI;
+	std::string PI;	
 	std::vector<std::string> true_minterm;
-} PIs;
+} PIs;	//Prime Implicant와 거기에 대응되는 True Minterm들을 저장
 
 bool ComparePIs(std::string pi, std::string minterm, int bit_length);
 LogicExpr FileRead(const char file_name[]);
 std::vector<PIs> MakeImplicantTable(LogicExpr& logic_expr);
-bool CompareMinterm(const StringData& data1, const StringData& data2);
 int FindHamOne(const std::string& str1, const std::string& str2, int bit_length);
 int FindHot(std::string minterm, int bit_length);
